dbd80fbfec03c0de960e482882be6817a36601bc
Minsu Lee <minsu.lee.it@gmail.com>
2017-06-12 22:05:05 -0300

    change theme

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,169 @@
-(function($) {
-    $(document).ready(function() {
-    /* IF YOU WANT TO APPLY SOME BASIC JQUERY TO REMOVE THE VIDEO BACKGROUND ON A SPECIFIC VIEWPORT MANUALLY
-     var is_mobile = false;
-    if( $('.player').css('display')=='none') {
-        is_mobile = true;       
-    }
-    if (is_mobile == true) {
-        //Conditional script here
-        $('.big-background, .small-background-section').addClass('big-background-default-image');
-    }else{
-        $(".player").mb_YTPlayer(); 
-    }
-    });
-*/
-    /*  IF YOU WANT TO USE DEVICE.JS TO DETECT THE VIEWPORT AND MANIPULATE THE OUTPUT  */
+/*global jQuery:false */
+(function ($) {
 
-        //Device.js will check if it is Tablet or Mobile - http://matthewhudson.me/projects/device.js/
-        if (!device.tablet() && !device.mobile()) {
-            $(".player").mb_YTPlayer();
-        } else {
-            //jQuery will add the default background to the preferred class 
-            $('.video-background').addClass(
-                'video-background-default-image');
-        }
+
+	$(window).load(function(){
+      $("#navigation").sticky({ topSpacing: 0 });
     });
+	
+
+	wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       0
+      }
+    );
+    wow.init();
+
+	$('ul.nav li.dropdown').hover(function() {
+	  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+	}, function() {
+	  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+	});	
+
+	
+	//jQuery to collapse the navbar on scroll
+	$(window).scroll(function() {
+		if ($(".navbar").offset().top > 50) {
+			$(".navbar-fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".navbar-fixed-top").removeClass("top-nav-collapse");
+		}
+	});
+	
+	//jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$('.navbar-nav li a').bind('click', function(event) {
+			var $anchor = $(this);
+			var nav = $($anchor.attr('href'));
+			if (nav.length) {
+			$('html, body').stop().animate({				
+				scrollTop: $($anchor.attr('href')).offset().top				
+			}, 1500, 'easeInOutExpo');
+			
+			event.preventDefault();
+			}
+		});
+		$('a.totop,a#btn-scroll,a.btn-slide').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+	
+	//home slider
+	jQuery('#valera-slippry').slippry({
+	pager: false
+	
+	});
+	
+	$('.testimonialslide').flexslider({
+	animation: "slide",
+	slideshow: false,
+	directionNav:false,
+	controlNav: true
+	});
+	
+	//owl carousel
+	$('#owl-works').owlCarousel({
+            items : 4,
+            itemsDesktop : [1199,5],
+            itemsDesktopSmall : [980,5],
+            itemsTablet: [768,5],
+            itemsTabletSmall: [550,2],
+            itemsMobile : [480,2],
+        });
+	
+	//nivo lightbox
+	$('.owl-carousel .item a').nivoLightbox({
+		effect: 'fadeScale',                             // The effect to use when showing the lightbox
+		theme: 'default',                           // The lightbox theme to use
+		keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
+		clickOverlayToClose: true,                  // If false clicking the "close" button will be the only way to close the lightbox
+		onInit: function(){},                       // Callback when lightbox has loaded
+		beforeShowLightbox: function(){},           // Callback before the lightbox is shown
+		afterShowLightbox: function(lightbox){},    // Callback after the lightbox is shown
+		beforeHideLightbox: function(){},           // Callback before the lightbox is hidden
+		afterHideLightbox: function(){},            // Callback after the lightbox is hidden
+		onPrev: function(element){},                // Callback when the lightbox gallery goes to previous item
+		onNext: function(element){},                // Callback when the lightbox gallery goes to next item
+		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
+	});
+	
+
+	jQuery('.appear').appear();
+	jQuery(".appear").on("appear", function(data) {
+			var id = $(this).attr("id");
+			jQuery('.nav li').removeClass('active');
+			jQuery(".nav a[href='#" + id + "']").parent().addClass("active");					
+		});
+		
+		//stats
+		var runOnce = true;
+		jQuery(".stats").on("appear", function(data) {
+			var counters = {};
+			var i = 0;
+			if (runOnce){
+				jQuery('.number').each(function(){
+					counters[this.id] = $(this).html();
+					i++;
+				});
+				jQuery.each( counters, function( i, val ) {
+					//console.log(i + ' - ' +val);
+					jQuery({countNum: 0}).animate({countNum: val}, {
+					  duration: 3000,
+					  easing:'linear',
+					  step: function() {
+						jQuery('#'+i).text(Math.floor(this.countNum));
+					  }
+					});
+				});
+				runOnce = false;
+			}
+		});
+		
+		//parallax
+        if ($('#parallax1').length  || $('#parallax2').length)
+        {
+			$(window).stellar({
+				responsive:true,
+                scrollProperty: 'scroll',
+                parallaxElements: false,
+                horizontalScrolling: false,
+                horizontalOffset: 0,
+                verticalOffset: 0
+            });
+
+        }
+		
+		//video bg
+		$(".bg-player").mb_YTPlayer();
+		
+	$("#js-rotating").Morphext({
+		// Animation type (refer to Animate.css for a list of available animations)
+		animation: "fadeInDown",
+		// An array of words to rotate are created based on this separator. Change it if you wish to separate the words differently (e.g. So Simple | Very Doge | Much Wow | Such Cool)
+		separator: ",",
+		// The delay between each word in milliseconds
+		speed: 3000
+	});
+		
+	//nicescroll
+	$("html").niceScroll({zindex:999,cursorborder:"",cursorborderradius:"0px",cursorwidth:"10px",cursorcolor:"#555",cursoropacitymin:.5});
+	function initNice() {
+		if($(window).innerWidth() <= 960) {
+			$('html').niceScroll().remove();
+		} else {
+			$("html").niceScroll({zindex:999,cursorborder:"",cursorborderradius:"2px",cursorcolor:"#555",cursoropacitymin:.1});
+		}
+	}
+	$(window).load(initNice);
+	$(window).resize(initNice);
+	
 })(jQuery);
+$(window).load(function() {
+	$(".loader").delay(300).fadeOut();
+	$("#page-loader").delay(500).fadeOut("slow");
+});
